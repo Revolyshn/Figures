e053320cba1e7350bf0247c063b94d46390169b9
Revolyshn <dkorcagin6@gmail.com>
2021-05-25 14:02:04 +0500

    Added Rhomb

---
+++ WindowsFormsApp5/Rhomb.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace WindowsFormsApp5
+{
+    class Rhomb:Shape
+    {
+		public Rhomb(Point[] p, Color color) : base(p, color)
+		{
+			if (p.Length != 4)
+			{
+				ERROR("ОШИБКА");
+			}
+		}
+	}
+}
